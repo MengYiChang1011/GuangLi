fc6114830acaf9463df421e415c9775ecf7da6aa
MengYiChang1011 <3287386207@qq.com>
2020-12-24 15:10:33 +0800

    这是第一次提交

---
+++ src/com/company/Student.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * @author zengshihao
+ * @version 1.0
+ * @description:
+ * @date 2020/12/24 14:52
+ */
+public class Student {
+
+    public static void main(String[] args) {
+        System.out.println("hello,World");
+    }
+
+}
